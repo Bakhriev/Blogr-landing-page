26775a31871c8997934d1af2b27e327217d2ba10
Bakhriev <legendofpain5112@gmail.com>
2023-01-12 23:39:01 +0500

    re build all

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,45 @@
-const headerMenuItems = document.querySelectorAll(".menuItem")
+function burgerMenu() {
+	const burger = document.querySelector(".burger")
+	const menu = document.querySelector(".menu")
+	const popup = document.querySelector(".popup")
 
-headerMenuItems.forEach(item => {
-	const itemSubMenu = item.querySelector(".subMenu")
-	item.addEventListener("click", () => {
-		itemSubMenu.classList.toggle("subMenuActive")
-		item.classList.toggle("menuItemRotate")
+	burger.addEventListener("click", () => {
+		if (!menu.classList.contains("menuActive")) {
+			menu.classList.add("menuActive")
+			burger.classList.add("burgerActive")
+			popup.classList.toggle("popupActive")
+		} else {
+			menu.classList.remove("menuActive")
+			burger.classList.remove("burgerActive")
+			popup.classList.remove("popupActive")
+		}
+	})
+
+	window.addEventListener("resize", () => {
+		if (window.innerWidth > 991.98) {
+			menu.classList.remove("menuActive")
+			burger.classList.remove("burgerActive")
+			popup.classList.remove("popupActive")
+		}
+	})
+}
+burgerMenu()
+
+document.addEventListener("click", e => {
+	const isDropdownButton = e.target.matches("[data-dropdown-button]")
+
+	// if clicked element === dropdown item don't close dropdown
+	if (!isDropdownButton && e.target.closest("[data-dropdown]") !== null) return
+
+	// and if clicked element is dropdownButton so then add active class
+	let currentDropdown
+	if (isDropdownButton) {
+		currentDropdown = e.target.closest("[data-dropdown]")
+		currentDropdown.classList.toggle("active")
+	}
+	//If  clicked another drop-down, then delete the active class from the current drop-down
+	document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+		if (dropdown === currentDropdown) return
+		dropdown.classList.remove("active")
 	})
 })
