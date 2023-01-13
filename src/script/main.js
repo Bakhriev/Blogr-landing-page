fd843cb4eb20663acbe09ee68e394b430c8f9dbe
Bakhriev <legendofpain5112@gmail.com>
2023-01-13 14:32:23 +0500

    complete popup dropdown animate

--- src/script/main.js
+++ src/script/main.js
@@ -2,16 +2,19 @@ function burgerMenu() {
 	const burger = document.querySelector(".burger")
 	const menu = document.querySelector(".menu")
 	const popup = document.querySelector(".popup")
+	const body = document.querySelector("body")
 
 	burger.addEventListener("click", () => {
 		if (!menu.classList.contains("menuActive")) {
 			menu.classList.add("menuActive")
 			burger.classList.add("burgerActive")
 			popup.classList.toggle("popupActive")
+			body.classList.toggle("locked")
 		} else {
 			menu.classList.remove("menuActive")
 			burger.classList.remove("burgerActive")
 			popup.classList.remove("popupActive")
+			body.classList.remove("locked")
 		}
 	})
 
@@ -20,17 +23,23 @@ function burgerMenu() {
 			menu.classList.remove("menuActive")
 			burger.classList.remove("burgerActive")
 			popup.classList.remove("popupActive")
+			body.classList.remove("locked")
 		}
 	})
 }
 burgerMenu()
 
-// const popupToggleButtons = document.querySelectorAll(".popup-menu-toggle")
-// popupToggleButtons.forEach(popuptoggle => {
-// 	popuptoggle.addEventListener("click", () => {
-// 		popuptoggle.classList.toggle("popup-menu-toggle-active")
-// 	})
-// })
+const popupDropdownButtons = document.querySelectorAll(".popup-menu-toggle")
+popupDropdownButtons.forEach(popupDropdownButton => {
+	popupDropdownButton.addEventListener("click", () => {
+		const popupSubMenu = popupDropdownButton.nextElementSibling
+		if (popupSubMenu.style.maxHeight) {
+			popupSubMenu.style.maxHeight = null
+		} else {
+			popupSubMenu.style.maxHeight = popupSubMenu.scrollHeight + "px"
+		}
+	})
+})
 
 document.addEventListener("click", e => {
 	const isDropdownButton = e.target.matches("[data-dropdown-button]")
